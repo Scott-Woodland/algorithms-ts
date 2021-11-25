9ce18f21009b2301c1e70f50db06311bf194409d
Scott Woodland <scott@wenet.com>
2021-11-25 09:36:20 +0000

    binary search

--- src/search/binary/binarySearch.ts
+++ src/search/binary/binarySearch.ts
@@ -1,7 +1,21 @@
 import { NO_MATCH } from "../common"
+let lastIndex = -2;
 
 const binarySearch = (items: any[], item: any): number => {
-
+    let min = 0;
+    let max = items.length;
+    for (let i = items.length/2; i != lastIndex; i = Math.floor(min+((max-min)/2))) {
+            if (item === items[i]){
+                return i;
+            }
+            else if (item < items[i]){
+                max = i;
+            }
+            else if (item > items[i]){
+                min = i;
+            }
+            lastIndex = i;
+    }        
     return NO_MATCH;
 }
 
