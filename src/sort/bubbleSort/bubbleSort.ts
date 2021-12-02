e6ab625312f9494dc1dee1bb8be9fbee01d636fc
Scott Woodland <scott@wenet.com>
2021-12-02 09:51:26 +0000

    bubblesort

--- src/sort/bubbleSort/bubbleSort.ts
+++ src/sort/bubbleSort/bubbleSort.ts
@@ -3,8 +3,26 @@ import swap from "../swap";
 
 const bubbleSort: SortAlgorithm<any> = <T>(items: T[]): T[] => {
 
-    // Your code here
+    let top = items.length;
+    let current = 0;
+    let sorts = 1;
+    let swapstore;
 
+    while (sorts > 0){
+        sorts = 0;
+        current = 0;
+        while(current != top){
+
+            if(items[current] > items[current+1]){
+                swapstore = items[current];
+                items[current] = items[current+1]
+                items[current+1] = swapstore;
+                sorts++;
+            }
+            current++;
+        }
+        top--;
+    }
     return items;
 }
 
